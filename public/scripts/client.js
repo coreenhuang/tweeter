d9f21a13dc764b073b91164b053dcf50836e739b
Coreen Huang <coreen.huang@gmail.com>
2022-09-15 23:03:58 -0400

    fetching tweets with ajax and time converter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,6 +19,8 @@ $(document).ready(function() {
 
   const createTweetElement = function(tweetData) {
 
+  const convertedTime = timeago.format(tweetData.created_at);
+
   return `
   <section class="posted-tweets">
     <header>
@@ -36,7 +38,7 @@ $(document).ready(function() {
 
     <footer>
       <div>
-        <p>${tweetData.created_at}</p>
+        <p>${convertedTime}</p>
       </div>
       <div>
         <i class="fa-solid fa-flag icons"></i>
@@ -55,33 +57,6 @@ $(document).ready(function() {
     }
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
   $('form').submit(function(event) {
     event.preventDefault();
     const textArea = $(this).children()[1];
@@ -91,5 +66,14 @@ $(document).ready(function() {
 
     console.log($(this).serialize());
   })
+
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweets) {
+      renderTweets(tweets);
+    });
+  }
+
+  loadTweets();
 
 });
