1421fd236a2ce17f4e41607397539ab5cf838ca2
Coreen Huang <coreen.huang@gmail.com>
2022-09-15 20:03:12 -0400

    counter update and red font negative value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   console.log('ready!');
 
-  $("#tweet-text").on('input', function() {
-    console.log($(this).val().length); 
+  $("textarea").on('input', function() {
+
+    let charTyped = $(this).val().length;
+
+    const counter = $(this).next().children()[1];
+
+    counter.innerHTML = 140 - charTyped;
+
+    if (charTyped > 140) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "#545149");
+    }
+
   });
 
 });
